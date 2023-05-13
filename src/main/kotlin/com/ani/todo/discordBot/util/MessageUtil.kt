68901045cdcf424d69c5dd4a78b1946d840fc770
ani2689 <ojno7777@gmail.com>
2023-05-13 20:15:55 +0900

    :recycle: util 간결화

--- src/main/kotlin/com/ani/todo/discordBot/util/MessageUtil.kt
+++ src/main/kotlin/com/ani/todo/discordBot/util/MessageUtil.kt
@@ -1,15 +1,14 @@
 package com.ani.todo.discordBot.util
 
-import com.ani.todo.discordBot.todo.exception.ErrorCode
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.User
+import net.dv8tion.jda.api.interactions.components.ActionRow
 
 interface MessageUtil {
     fun info(): EmbedBuilder
     fun todoList(user: User): EmbedBuilder
-    fun createTodo(): EmbedBuilder
-    fun deliteTodo(): EmbedBuilder
-    fun clearTodo(): EmbedBuilder
-    fun error(errorCode: ErrorCode): EmbedBuilder
-    fun ckeck(): EmbedBuilder
+    fun createTodo(title: String, user: User): EmbedBuilder
+    fun deleteTodo(): EmbedBuilder
+    fun completeTodo(): EmbedBuilder
+    fun check(): ActionRow
 }
