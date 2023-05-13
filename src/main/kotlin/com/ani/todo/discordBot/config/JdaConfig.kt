964063dde424bd57ed9b1336e02245b3c2fe6a9d
ani2689 <ojno7777@gmail.com>
2023-05-13 20:14:14 +0900

    :recycle: config에서 봇 추가

--- src/main/kotlin/com/ani/todo/discordBot/config/JdaConfig.kt
+++ src/main/kotlin/com/ani/todo/discordBot/config/JdaConfig.kt
@@ -1,8 +1,10 @@
 package com.ani.todo.discordBot.config
 
 import com.ani.todo.discordBot.listener.BotListener
+import com.ani.todo.discordBot.util.MessageUtil
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.requests.GatewayIntent
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -13,11 +15,13 @@ import org.springframework.context.annotation.Configuration
 class JdaConfig (
     @Value("\${discord.bot.token}")
     private val botToken: String,
+    botListener: BotListener
 ){
 
     private val jda = JDABuilder.createDefault(botToken)
-        .enableIntents(GatewayIntent.MESSAGE_CONTENT)
-        .addEventListeners(BotListener())
+        .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES)
+        .addEventListeners(botListener)
+        .setActivity(Activity.playing("해야 할 일 재촉"))
         .build()
         .awaitReady()
 
