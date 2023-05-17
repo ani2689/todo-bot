adb7d5881d15bef7ad2373656a57c329180ccc0e
ani2689 <ojno7777@gmail.com>
2023-05-17 21:15:01 +0900

    :fire: 필요없는 메서드 삭제

--- src/main/kotlin/com/ani/todo/discordBot/util/MessageUtil.kt
+++ src/main/kotlin/com/ani/todo/discordBot/util/MessageUtil.kt
@@ -7,8 +7,4 @@ import net.dv8tion.jda.api.interactions.components.ActionRow
 interface MessageUtil {
     fun info(): EmbedBuilder
     fun todoList(user: User): EmbedBuilder
-    fun createTodo(title: String, user: User): EmbedBuilder
-    fun deleteTodo(): EmbedBuilder
-    fun completeTodo(): EmbedBuilder
-    fun check(): ActionRow
 }
---
+++ src/main/kotlin/com/ani/todo/discordBot/util/MessageUtilImpl.kt
@@ -0,0 +1,37 @@
+package com.ani.todo.discordBot.util
+
+import com.ani.todo.discordBot.todo.entity.Todo
+import com.ani.todo.discordBot.todo.entity.status.TodoStatus
+import com.ani.todo.discordBot.todo.repository.TodoRepository
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.User
+import net.dv8tion.jda.api.interactions.components.ActionRow
+import org.springframework.stereotype.Component
+
+@Component
+class MessageUtilImpl(
+    private val todoRepository: TodoRepository
+): MessageUtil {
+    override fun info(): EmbedBuilder = EmbedBuilder()
+        .addField("!할 일 {유저 이름}", "호출된 유저의 할 일 목록을 가져옵니다.", false)
+
+    override fun todoList(user: User): EmbedBuilder {
+        val doneList = ArrayList<Todo>()
+        val stayList = ArrayList<Todo>()
+        todoRepository.findByUserId(user.id)
+            .forEach{
+                when(it.status) {
+                TodoStatus.DONE-> doneList.add(it)
+                TodoStatus.STAY -> stayList.add(it)
+            }}
+
+        val stayListString = if (stayList.isNotEmpty()) "**${stayList.joinToString("\n") { it.title }}**" else ""
+        val doneListString = if (doneList.isNotEmpty()) "~~${doneList.joinToString("\n") { it.title }}~~" else ""
+
+        return EmbedBuilder()
+            .addField("할 일 목록", when(doneList.isEmpty()&&stayList.isEmpty()) {
+                true ->     "작성된 할 일이 없어요."
+                false ->    stayListString + doneListString
+            },true)
+    }
+}
