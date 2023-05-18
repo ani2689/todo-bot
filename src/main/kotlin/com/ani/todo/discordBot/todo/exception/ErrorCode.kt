a436344701bf169bb2d7f23bdea45218d883a2a5
ani2689 <ojno7777@gmail.com>
2023-05-18 15:47:21 +0900

    :fire: 안쓰는 에러 삭제

--- src/main/kotlin/com/ani/todo/discordBot/todo/exception/ErrorCode.kt
+++ src/main/kotlin/com/ani/todo/discordBot/todo/exception/ErrorCode.kt
@@ -6,11 +6,4 @@ enum class ErrorCode (
 ){
     INVALID_COMMAND("유효한 명령어가 아니에요.", 400),
 
-    NOT_AUTHOR("본인만 편집할 수 있어요.",403),
-
-    NOT_FOUND_TODO("존재하지 않는 할 일이에요.",404),
-    NOT_FOUND_USER("존재하지 않는 사람이에요.",404),
-
-    TIME_OUT("오래된 요청입니다. 봇을 다시 불러주세요.",408),
-
 }
