c802bec0a94a5fd5109a03a76f9a293b2243b543
ani2689 <ojno7777@gmail.com>
2023-07-11 23:26:08 +0900

    :sparkles: 알람 레포지토리 추가

---
+++ src/main/kotlin/com/ani/todo/discordBot/todo/repository/AlarmRepository.kt
@@ -0,0 +1,10 @@
+package com.ani.todo.discordBot.todo.repository
+
+import com.ani.todo.discordBot.todo.entity.Alarm
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface AlarmRepository : CrudRepository<Alarm, Long> {
+    fun findByTitleAndChannelId(title: String, channelId: String): Alarm
+}
