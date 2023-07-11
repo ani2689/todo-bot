f09662bf81579d9c6e80f9cd75d252c5951cdc31
ani2689 <ojno7777@gmail.com>
2023-07-11 23:27:12 +0900

    :recycle: 이름 변경

--- src/main/kotlin/com/ani/todo/discordBot/util/Schedule.kt
+++ src/main/kotlin/com/ani/todo/discordBot/util/Schedule.kt
@@ -5,7 +5,7 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-class InitTodo (
+class Schedule (
     private val todoRepository: TodoRepository
 ){
     @Scheduled(cron = "0 0 0 1/1 * ? *")
