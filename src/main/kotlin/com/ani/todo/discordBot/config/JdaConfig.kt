a4b3cc7cca9032d8d4a22bb19e93323f4b95376e
ani2689 <ojno7777@gmail.com>
2023-07-12 09:16:03 +0900

    :recycle: 명령어 한글로 변경

--- src/main/kotlin/com/ani/todo/discordBot/config/JdaConfig.kt
+++ src/main/kotlin/com/ani/todo/discordBot/config/JdaConfig.kt
@@ -33,34 +33,33 @@ class JdaConfig (
 
 
     init {
-        jda.upsertCommand("help", "봇의 명령어를 가져옵니다.").queue()
-        jda.upsertCommand("todo", "대상의 할 일 목록을 가져옵니다.")
-            .addOptions(OptionData(OptionType.USER, "user", "대상을 지정해 주세요.")
+        jda.upsertCommand("도움말", "봇의 명령어를 가져옵니다.").queue()
+        jda.upsertCommand("할일", "대상의 할 일 목록을 가져옵니다.")
+            .addOptions(OptionData(OptionType.USER, "대상", "대상을 지정해 주세요.")
                 .setRequired(false))
             .queue()
-        jda.upsertCommand("add", "할 일을 추가합니다.")
-            .addOptions(OptionData(OptionType.STRING, "todo", "추가할 할 일을 작성해 주세요.")
+        jda.upsertCommand("할일추가", "할 일을 추가합니다.")
+            .addOptions(OptionData(OptionType.STRING, "할일", "추가할 할 일을 작성해 주세요.")
                 .setRequired(true))
             .queue()
-        jda.upsertCommand("complete", "완료할 할 일을 선택합니다.").queue()
-        jda.upsertCommand("daily", "데일리를 작성합니다.")
+        jda.upsertCommand("할일완료", "할 일을 완료합니다").queue()
+        jda.upsertCommand("데일리", "데일리를 작성합니다.")
             .addOptions(
-                OptionData(OptionType.STRING, "yesterday_task", "어제 한 일을 작성해 주세요.").setRequired(true),
-                OptionData(OptionType.STRING, "today_task", "오늘 할 일을 작성해 주세요.").setRequired(true),
-                OptionData(OptionType.STRING, "hard_task", "어려웠던 점을 작성해 주세요.").setRequired(true),
-                OptionData(OptionType.STRING, "share", "링크를 남겨주세요.").setRequired(false)
+                OptionData(OptionType.STRING, "어제한일", "어제 한 일을 작성해 주세요.").setRequired(true),
+                OptionData(OptionType.STRING, "오늘하일", "오늘 할 일을 작성해 주세요.").setRequired(true),
+                OptionData(OptionType.STRING, "어려웠던점", "어려웠던 점을 작성해 주세요.").setRequired(true),
+                OptionData(OptionType.STRING, "공유", "링크를 남겨주세요.").setRequired(false)
             ).queue()
-        jda.upsertCommand("loud", "알람을 추가합니다.")
+        jda.upsertCommand("알람추가", "알람을 추가합니다.")
             .addOptions(
-                OptionData(OptionType.STRING, "title", "알람의 제목을 설정합니다.").setRequired(true),
-                OptionData(OptionType.CHANNEL, "channel", "알람을 보낼 채널을 설정합니다.").setRequired(true),
-                OptionData(OptionType.ROLE, "role", "알람을 보낼 대상의 역할을 설정합니다.").setRequired(false),
-                OptionData(OptionType.STRING, "content", "알람의 내용을 설정합니다.").setRequired(false)
+                OptionData(OptionType.STRING, "제목", "알람의 제목을 설정합니다.").setRequired(true),
+                OptionData(OptionType.CHANNEL, "채널", "알람을 보낼 채널을 설정합니다.").setRequired(true),
+                OptionData(OptionType.ROLE, "역할", "알람을 보낼 대상의 역할을 설정합니다.").setRequired(false),
+                OptionData(OptionType.STRING, "내용", "알람의 내용을 설정합니다.").setRequired(false)
             ).queue()
-        jda.upsertCommand("silence", "알람을 삭제합니다.")
+        jda.upsertCommand("알람삭제", "알람을 삭제합니다.")
             .addOptions(
-                OptionData(OptionType.STRING, "title", "지울 알람의 제목을 설정합니다.").setRequired(true),
-                OptionData(OptionType.STRING, "channel", "지울 알람이 있는 채널을 설정합니다.").setRequired(true)
+                OptionData(OptionType.CHANNEL, "채널", "지울 알람이 있는 채널을 설정합니다.").setRequired(true)
             ).queue()
 
 
