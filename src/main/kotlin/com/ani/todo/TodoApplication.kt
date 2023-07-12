ce286950434cf29330dfb5913a4a5c0ccebbfcb0
ani2689 <ojno7777@gmail.com>
2023-07-12 10:28:34 +0900

    :sparkles: 알람 신청한 채널에 8시 30분 일괄 알람 보내기

--- src/main/kotlin/com/ani/todo/TodoApplication.kt
+++ src/main/kotlin/com/ani/todo/TodoApplication.kt
@@ -2,8 +2,9 @@ package com.ani.todo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
-
+@EnableScheduling
 @SpringBootApplication
 class TodoApplication
 fun main(args: Array<String>) {
--- src/main/kotlin/com/ani/todo/discordBot/util/Schedule.kt
+++ src/main/kotlin/com/ani/todo/discordBot/util/Schedule.kt
@@ -1,16 +1,41 @@
 package com.ani.todo.discordBot.util
 
+import com.ani.todo.discordBot.todo.repository.AlarmRepository
 import com.ani.todo.discordBot.todo.repository.TodoRepository
+import net.dv8tion.jda.api.JDA
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
 class Schedule (
-    private val todoRepository: TodoRepository
+    private val todoRepository: TodoRepository,
+    private val alarmRepository: AlarmRepository,
+    private val messageUtil: MessageUtil,
+    private val jda: JDA
 ){
-    @Scheduled(cron = "0 0 0 1/1 * ? *")
+    @Scheduled(cron = "0 0 0 * * *")
     fun initTodo(){
         todoRepository.findAll()
             .map { todoRepository.delete(it) }
+    }
+
+    @Scheduled(cron = "30 8 * * * *")
+    fun callAlarm() {
+        alarmRepository.findAll()
+            .map {
+                when (val channel = jda.getTextChannelById(it.channelId)) {
+                    null -> alarmRepository.delete(it)
+                    else ->
+                        if (it.role == null || jda.getRoleById(it.role!!) == null)
+                            channel.sendMessage("")
+                                .setEmbeds(messageUtil.alarm(it.title, it.content).build())
+                                .queue()
+                        else
+                            channel.sendMessage(jda.getRoleById(it.role!!)!!.asMention)
+                                .setEmbeds(messageUtil.alarm(it.title, it.content).build())
+                                .queue()
+
+                }
+            }
     }
 }
