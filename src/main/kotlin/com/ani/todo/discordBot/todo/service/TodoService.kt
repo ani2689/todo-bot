dc451479fa4e703b7eab0142af069d80cfba9d08
ani2689 <ojno7777@gmail.com>
2023-12-06 14:19:26 +0900

    :sparkles: 메서드 추가

--- src/main/kotlin/com/ani/todo/discordBot/todo/service/TodoService.kt
+++ src/main/kotlin/com/ani/todo/discordBot/todo/service/TodoService.kt
@@ -5,5 +5,6 @@ import com.ani.todo.discordBot.todo.data.*
 interface TodoService {
     fun createTodo(request: CreateTodoRequest): CreateTodoResponse
     fun queryTodo(request: QueryTodoRequest): QueryTodoResponse
+    fun choiceTodo(request: ChoiceTodoRequest): ChoiceTodoResponse
     fun checkTodo(request: CheckTodoRequest): CheckTodoResponse
 }
