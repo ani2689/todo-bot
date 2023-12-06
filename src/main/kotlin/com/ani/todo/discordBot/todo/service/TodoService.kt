6b012a974a853d78f1ae51a38ff88bc0a6de568b
ani2689 <ojno7777@gmail.com>
2023-12-06 11:25:35 +0900

    :sparkles: 할일 추가 서비스

--- src/main/kotlin/com/ani/todo/discordBot/todo/service/TodoService.kt
+++ src/main/kotlin/com/ani/todo/discordBot/todo/service/TodoService.kt
@@ -1,11 +1,9 @@
 package com.ani.todo.discordBot.todo.service
 
-import com.ani.todo.discordBot.todo.data.CheckTodoRequest
-import com.ani.todo.discordBot.todo.data.CheckTodoResponse
-import com.ani.todo.discordBot.todo.data.QueryTodoRequest
-import com.ani.todo.discordBot.todo.data.QueryTodoResponse
+import com.ani.todo.discordBot.todo.data.*
 
 interface TodoService {
+    fun createTodo(request: CreateTodoRequest): CreateTodoResponse
     fun queryTodo(request: QueryTodoRequest): QueryTodoResponse
     fun checkTodo(request: CheckTodoRequest): CheckTodoResponse
 }
