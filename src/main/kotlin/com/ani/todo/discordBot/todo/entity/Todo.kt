d83720249fc334b975f6318b644d0cc0d6c84ce7
ani2689 <ojno7777@gmail.com>
2023-12-06 11:53:44 +0900

    :recycle: 객체 리팩토링

--- src/main/kotlin/com/ani/todo/discordBot/todo/entity/Todo.kt
+++ src/main/kotlin/com/ani/todo/discordBot/todo/entity/Todo.kt
@@ -1,32 +1,23 @@
 package com.ani.todo.discordBot.todo.entity
 
 import com.ani.todo.discordBot.todo.entity.status.TodoStatus
-import lombok.Getter
-import lombok.Setter
 import org.hibernate.annotations.DynamicUpdate
 import javax.persistence.*
 
 @Entity
 @DynamicUpdate
-@Getter
-@Setter
 @Table(name = "todo")
 data class Todo (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "todo_id")
-    var id: Long = 0,
+    val id: Long = 0,
 
     @Column(name = "user_id")
-    var userId: String,
+    val userId: String,
 
-    var title: String,
+    val content: String,
 
     @Enumerated(value = EnumType.STRING)
-    var status: TodoStatus
-){
-    fun completeTodo(): Todo{
-        status = TodoStatus.DONE
-        return this
-    }
-}
+    val status: TodoStatus = TodoStatus.STAY
+)
