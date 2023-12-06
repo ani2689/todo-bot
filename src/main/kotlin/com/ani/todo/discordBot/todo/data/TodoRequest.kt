01bb73d219ba3747fd9bfbae2af40c8c7aa40ffa
ani2689 <ojno7777@gmail.com>
2023-12-06 14:19:05 +0900

    :sparkles: data

---
+++ src/main/kotlin/com/ani/todo/discordBot/todo/data/TodoRequest.kt
@@ -0,0 +1,20 @@
+package com.ani.todo.discordBot.todo.data
+
+import net.dv8tion.jda.api.entities.User
+
+data class CreateTodoRequest(
+    val user: User,
+    val content: String
+)
+
+data class QueryTodoRequest(
+    val user: User
+)
+
+data class ChoiceTodoRequest(
+    val user: User
+)
+
+data class CheckTodoRequest(
+    val user: User
+)
---
+++ src/main/kotlin/com/ani/todo/discordBot/todo/data/TodoResponse.kt
@@ -0,0 +1,25 @@
+package com.ani.todo.discordBot.todo.data
+
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.interactions.components.buttons.Button
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu
+
+data class CreateTodoResponse(
+    val content: String,
+)
+
+data class QueryTodoResponse(
+    val content: String,
+    val embed: EmbedBuilder,
+    val button: List<Button>
+)
+
+data class ChoiceTodoResponse(
+    val content: String,
+    val selectMenu: StringSelectMenu?
+)
+
+data class CheckTodoResponse(
+    val content: String,
+    val selectMenu: StringSelectMenu?
+)
