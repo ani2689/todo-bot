7f7f36b5f975b21fb200ecdc78c38e8ce249291d
ani2689 <ojno7777@gmail.com>
2023-12-06 15:39:21 +0900

    :fire: 에러코드 삭제

--- src/main/kotlin/com/ani/todo/discordBot/todo/exception/ErrorCode.kt
+++
@@ -1,9 +0,0 @@
-package com.ani.todo.discordBot.todo.exception
-
-enum class ErrorCode (
-    val title: String,
-    val status: Int
-){
-    INVALID_COMMAND("유효한 명령어가 아니에요.", 400), USER_NOT_FOUND("유효한 유저를 찾을 수 없어요.", 404)
-
-}
