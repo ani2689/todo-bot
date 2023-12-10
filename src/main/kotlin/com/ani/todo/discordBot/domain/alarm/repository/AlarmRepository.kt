33667ca69bce08695fb683d23b03e4272590b67c
ani2689 <ojno7777@gmail.com>
2023-12-10 17:37:24 +0900

    :recycle: 스케쥴러 리팩토링

--- src/main/kotlin/com/ani/todo/discordBot/domain/alarm/repository/AlarmRepository.kt
+++ src/main/kotlin/com/ani/todo/discordBot/domain/alarm/repository/AlarmRepository.kt
@@ -9,5 +9,5 @@ interface AlarmRepository : CrudRepository<Alarm, Long> {
     fun findByTitleAndChannelId(title: String, channelId: String): Alarm?
     fun existsByTitleAndChannelId(title: String, channelId: String): Boolean
     fun findByChannelId(channelId: String): List<Alarm>
-    fun findByTime(time: String): List<Alarm>
+    fun findAllByTime(time: String): List<Alarm>
 }
